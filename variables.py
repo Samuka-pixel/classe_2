1d31177aea392ddf4dd554dde5ddddcb9ba41b64
Samuka-pixel <shark06778@sharkcoders.pt>
2024-10-12 10:09:49 +0100

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+n1 = int (input("qual o primeiro número?"))
+n2 = int (input("qual o segundo número?"))
+x = n1 * n2
+y = n1 / n2
+z = n1 - n2
+a = n1 + n2
+#print()
+print("O resultado da multiplicação é", x)
+#print()
+print("O resultado da divisão é", y)
+print()
+print("O resultado da subtração é", z)
+print()
+print("O resultado da soma é", a)
